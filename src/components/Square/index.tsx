5569a4e65c50e404c5520c584f4f2f543db98f5e
Jasmine Hendricks <jhendricks@mparticle.com>
2024-06-28 14:33:57 -0600

    Square compenent init

---
+++ src/components/Square/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+const Square = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        boxSizing: "border-box",
+        border: "1px solid black",
+      }}
+    >
+      <button
+        className="pushable button"
+        >
+        <span className="shadow"></span>
+        <span className="edge"></span>
+        <span className="front">💣❓</span>
+      </button>
+    </div>
+  );
+};
+
+export default Square;
