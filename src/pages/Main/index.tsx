f308774dcd097a0a7b3e4b2d9eb8af7516e36703
Jasmine Hendricks <jhendricks@mparticle.com>
2024-06-28 14:34:31 -0600

    Main commit

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Square from '../../components/Square/index'
+
+const Main = (): JSX.Element  => {
+  return (
+    <div>
+      <div>I Guess This Is Minesweeper</div>
+      <Square />
+    </div>
+  )
+}
+
+export default Main
