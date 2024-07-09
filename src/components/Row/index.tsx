5dfb6b8f426e0461363c09432ff0cd52cae82dba
Jasmine Hendricks <jhendricks@mparticle.com>
2024-07-09 00:50:32 -0600

    nominal cleanup

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,9 +1,4 @@
 import React, { PropsWithChildren } from "react";
-import Square from "../Square";
-
-const NUM_OF_COLUMNS: number = 10;
-
-const someArray = Array(10);
 
 const Row = ({children}: PropsWithChildren) => {
   return (
